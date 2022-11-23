cea1f31cacb0de8b3299e20c7dfca8834f02b9c6
Pratik Pujari <pratikpujari1000@gmail.com>
2022-11-24 01:49:18 +0530

    removed sign up

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,21 +2,21 @@ const assert = require("assert");
 // signup test
 const SERVER_API_URL = "http://localhost:8000/api";
 
-test("signup", async () => {
-  const res = await fetch(`${SERVER_API_URL}/user/signup`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      firstName: "Pratik",
-      lastName: "Pujari",
-      email: "pratikpujari1000@gmail.com",
-      password: "Pratik@123",
-    }),
-  });
-  assert.equal(res.status, 201);
-});
+// test("signup", async () => {
+//   const res = await fetch(`${SERVER_API_URL}/user/signup`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       firstName: "Pratik",
+//       lastName: "Pujari",
+//       email: "pratikpujari1000@gmail.com",
+//       password: "Pratik@123",
+//     }),
+//   });
+//   assert.equal(res.status, 201);
+// });
 
 // login test
 test("login", async () => {
